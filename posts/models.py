31a7ff5e72e24e19a270fdc9d691235fd4e62796
Shishir Jha <shishirjhaccr@gmail.com>
2020-03-06 05:27:37 +0530

    added database models,admin templates

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    text = models.TextField()
+
+    # returns the post as the first 50 characters written in the message.
+    def __str__(self):
+        return self.text[:50]
